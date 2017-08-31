c0d6d18633d106f2f3b2804a2d37e599b2e918fb
Mario Zamora <maza1387@gmail.com>
2017-08-30 22:32:08 -0700

    Read view file, Inject file with dynamic values

--- renderer.js
+++ renderer.js
@@ -1,11 +1,22 @@
 var fs = require('fs');
 
+function mergeValues(values, content) {
+  // cycle over object keys
+  for(var key in values) {
+    // replace all {{keys}} with the value from the values object
+    content = content.replace('{{' + key + '}}', values[key])
+  }
+
+  // return merged content
+  return content;
+}
+
 function view(templateName, values, response) {
   // read from the template files
-  var fileContents = fs.readFileSync('./views/' + templateName + '.html');
-  
-  // insert values into the content
+  var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: 'utf8'});
 
+  // insert values into the content
+  fileContents = mergeValues(values, fileContents);
   // write out the response
     response.write(fileContents);
 }

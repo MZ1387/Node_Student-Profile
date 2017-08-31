0630de7a1e67d1f42ff6b098860e283ad45b5f4e
Mario Zamora <maza1387@gmail.com>
2017-08-30 20:37:09 -0700

    Create routes for home and user

--- app.js
+++ app.js
@@ -3,19 +3,46 @@
 
 // 1. create a web server
 
+var http = require('http');
+var PORT = 3000;
+
+http.createServer(function (request, response) {
+  homeRoute(request, response);
+
+}).listen(PORT, '127.0.0.1');
+
+console.log('Server running on PORT:', PORT);
+
 // 2. handle http route GET / and POST /
+function homeRoute(request, response) {
   //if the url = '/' && GET
+  if (request.url === "/") {
     // show search field
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+  }
   //if url = '/' && POST
     // redirect to /:username
+}
 
 // 3. handle http route GET /:username
-  // if url ='/...'
-    // get json from treehouse
-      // on 'end'
-        // show profile
-      // on 'error'
-        // show error
+  function userRoute(request, response) {
+    // if url ='/...'
+    var username = request.url.replace("/", "");
+    if (username.length > 0) {
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.write("Header\n");
+      response.write(username + "\n");
+      response.end("Footer\n");
+      // get json from treehouse
+        // on 'end'
+          // show profile
+        // on 'error'
+          // show error
+    }
+  }
 
 // 4. function that handles the reading of files and merges in values
   // read from file and get a string
